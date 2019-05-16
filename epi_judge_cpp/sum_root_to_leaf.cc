4cb5b3db62b30fa254cf15fd9b0ed1053340a054
ibm <iamindrajitb@gmail.com>
2019-05-16 07:24:49 +0530

    Sum to root

--- epi_judge_cpp/sum_root_to_leaf.cc
+++ epi_judge_cpp/sum_root_to_leaf.cc
@@ -1,9 +1,16 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+int sum(const unique_ptr<BinaryTreeNode<int>>& tree, int partial_sum) {
+  if(tree == NULL)
+    return 0;
+  partial_sum = partial_sum * 2 + tree->data;
+  if(tree->left == NULL && tree->right == NULL)
+    return partial_sum;
+  return (sum(tree->left, partial_sum) + sum(tree->right, partial_sum));
+}
 
 int SumRootToLeaf(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return 0;
+  return sum(tree,0);
 }
 
 int main(int argc, char* argv[]) {
