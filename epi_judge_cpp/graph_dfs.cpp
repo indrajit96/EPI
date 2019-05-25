1471fbad8595ea261863c5f54e216c32204c512a
ibm <iamindrajitb@gmail.com>
2019-05-25 10:45:11 +0530

    BFS

--- epi_judge_cpp/graph_dfs.cpp
+++ epi_judge_cpp/graph_dfs.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <list>
+#include <queue>
+
 using namespace std;
 
 class Graph {
   int V;
   list <int> * adj;
+  queue <int> q;
   void Dfs_helper(int v, bool visit[]);
   public :
   Graph(int V);
   void AddEdge(int v , int w);
   void Dfs(int v);
+  void Bfs(int v);
 };
 
 Graph :: Graph(int v) {
@@ -33,12 +37,38 @@ void Graph :: Dfs_helper(int v, bool visit[]) {
 }
 
 void Graph::Dfs(int v) {
-  bool * visit = new bool[v];
+  bool * visit = new bool[V];
   int i;
-  for(i=0;i<v;i++)
+  for(i=0;i<V;i++)
     visit[i] = false;
 
   Dfs_helper(v,visit);
+}
+
+void Graph::Bfs(int v) {
+  bool * visit = new bool[V];
+  int i;
+  for(i=0;i<V;i++)
+    visit[i] = false;
+
+  visit[v] = true;
+  q.push(v);
+
+  while(!q.empty()) {
+    v = q.front();
+    q.pop();
+
+    cout << v << " ";
+    visit[v] = true;
+
+    list<int>::iterator i;
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) {
+      if(!visit[*i]) {
+        q.push(*i);
+        visit[*i] = true;
+      }
+    }
+  }
 }
 
 int main () {
@@ -53,6 +83,8 @@ int main () {
   cout << "Following is Depth First Traversal"
             " (starting from vertex 2) \n"; 
   g.Dfs(2);
+  cout << "Following is B First Traversal \n";
+  g.Bfs(2);  
  return 0; 
 }
 
