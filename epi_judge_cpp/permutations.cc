9e4ec62d1232a96240bd6ebe5bce3ec88de9dcfc
ibm <iamindrajitb@gmail.com>
2019-06-12 08:13:11 +0530

    Partial soultion for combinations

--- epi_judge_cpp/permutations.cc
+++ epi_judge_cpp/permutations.cc
@@ -2,9 +2,24 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+void helper(int i, vector<int> *A_ptr, vector<vector<int>> *result) {
+  vector<int> &A = *A_ptr;
+  if(i == A.size() -1) {
+    result->emplace_back(A);
+    return;
+  }
+  for(int j = i; j <A.size(); ++j) {
+    std::swap(A[i],A[j]);
+    helper(j+1,A_ptr,result);
+    std::swap(A[i],A[j]);
+  }
+}
+
 vector<vector<int>> Permutations(vector<int> A) {
+  vector<vector<int>> result;
+  helper(0,&A,&result);
   // TODO - you fill in here.
-  return {};
+  return result;
 }
 
 int main(int argc, char* argv[]) {
