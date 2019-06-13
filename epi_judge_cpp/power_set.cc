aa0a14bd40a7a127ec567f0a39acec5ea2a7145b
ibm <iamindrajitb@gmail.com>
2019-06-13 09:42:22 +0530

    SUBSET by BACKTRACKING

--- epi_judge_cpp/power_set.cc
+++ epi_judge_cpp/power_set.cc
@@ -2,9 +2,22 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+void helper (const vector<int>& input_set, int dec_pointer, vector<int> * selected, vector<vector<int>>* power_set) {
+ if(dec_pointer == input_set.size()) {
+   power_set->emplace_back(*selected);
+   return;
+ }
+ selected->push_back(input_set[dec_pointer]);
+ helper(input_set,dec_pointer+1,selected,power_set);
+ selected->pop_back();
+ helper(input_set,dec_pointer+1,selected,power_set);
+}
+
 vector<vector<int>> GeneratePowerSet(const vector<int>& input_set) {
+  vector<vector<int>> power_set;
+  helper(input_set,0,new vector<int>,&power_set);
   // TODO - you fill in here.
-  return {};
+  return power_set;
 }
 
 int main(int argc, char* argv[]) {
