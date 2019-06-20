b9b0fa02e94b7b9deea9f6573c8b7aa4eb518926
ibm <iamindrajitb@gmail.com>
2019-06-20 11:33:31 +0530

    KnapSack TODO

--- epi_judge_cpp/fail_sum.cc
+++ epi_judge_cpp/fail_sum.cc
@@ -1,28 +1,35 @@
-bool driver(vector<vector<int> > *result,vector<int> &A, int B, vector<int> * selected, int pointer, int sum) {
+
+
+
+
+void driver(vector<vector<int> > *result,vector<int> &A, int B, vector<int> * selected, int pointer) {
     if(pointer == A.size()) {
-        return false;
+        vector<int> tmp;
+        tmp = *selected;
+        int sum =0;
+        for(int i = 0; i< selected->size(); i++)
+            sum = sum + tmp[i];
+        if(sum == B)
+            result->emplace_back(*selected);
+        return;
     }
-    if(sum == B ) {
-        result->emplace_back(*selected);
-        return true;
-    }
-    if( sum > B) {
-        return false;
-    }
-    selected->emplace_back(A[pointer]);
-    driver(result, A, B, selected, pointer + 1, sum + A[pointer]);
+    selected->push_back(A[pointer]);
+    driver(result, A, B, selected, pointer + 1);
     selected->pop_back();
-    driver(result, A, B, selected, pointer + 1, sum);
-    return true;
+    driver(result, A, B, selected, pointer + 1);
 }
 vector<vector<int> > Solution::combinationSum(vector<int> &A, int B) {
     vector<vector<int> > result;
     std::sort(A.begin(),A.end());
-    driver(&result,A,B, new vector<int>,1,0);
+    driver(&result,A,B, new vector<int>,0);
+    std::sort(result.begin(), result.end());
+    auto it = std::unique(result.begin(), result.end());
+    result.resize( std::distance(result.begin(),it) );
+
+//    result.erase(std::unique(result.begin(), result.end()), result.end());
     return result;
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
     // Do not print the output, instead return values as specified
     // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
 }
-
--- epi_judge_cpp/knapsack.cc
+++ epi_judge_cpp/knapsack.cc
@@ -9,6 +9,27 @@ struct Item {
 
 int OptimumSubjectToCapacity(const vector<Item>& items, int capacity) {
   // TODO - you fill in here.
+  int size;
+  size = items.size();
+  int result[size][size];
+  int i,j;
+  for(i=0;i<size;i++) {
+    result[0][i] = 0;
+    result[i][0] = 0;
+    if(i>= items[0].weight)
+      result[0][i] = items[0].weight;
+  }
+  for(i=0;i<size;i++) {
+    if(1>= items[i].weight)
+      result[i][0] = items[0].weight;
+  }
+  for(i=1;i<size;i++) {
+    for(j=1;j<size;j++) {
+      int new_val = 0;
+      if(result[i-1][j] + items[i].weight
+      result[i][j] = std::max(
+
+
   return 0;
 }
 template <>
