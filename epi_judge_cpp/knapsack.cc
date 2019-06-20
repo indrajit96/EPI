c88939c991330ca3e8097eab5d83f839529d36da
ibm <iamindrajitb@gmail.com>
2019-06-20 12:27:30 +0530

    Knapsack Fixed

--- epi_judge_cpp/knapsack.cc
+++ epi_judge_cpp/knapsack.cc
@@ -11,26 +11,27 @@ int OptimumSubjectToCapacity(const vector<Item>& items, int capacity) {
   // TODO - you fill in here.
   int size;
   size = items.size();
-  int result[size][size];
+  int result[size][capacity+1];
   int i,j;
   for(i=0;i<size;i++) {
-    result[0][i] = 0;
     result[i][0] = 0;
-    if(i>= items[0].weight)
-      result[0][i] = items[0].weight;
   }
-  for(i=0;i<size;i++) {
-    if(1>= items[i].weight)
-      result[i][0] = items[0].weight;
+  for(i=0;i<=capacity;i++) {
+    if(i>= items[0].weight)
+      result[0][i] = items[0].value;
+    else
+      result[0][i] = 0;
   }
   for(i=1;i<size;i++) {
-    for(j=1;j<size;j++) {
+    for(j=1;j<=capacity;j++) {
       int new_val = 0;
-      if(result[i-1][j] + items[i].weight
-      result[i][j] = std::max(
-
-
-  return 0;
+      if(items[i].weight <= j)
+        new_val = items[i].value + result[i-1][j-items[i].weight];
+      result[i][j] = std::max(new_val,result[i][j-1]);
+      result[i][j] = std::max(result[i][j],result[i-1][j]);
+    }
+  }
+  return result[size-1][capacity];
 }
 template <>
 struct SerializationTraits<Item> : UserSerTraits<Item, int, int> {};
