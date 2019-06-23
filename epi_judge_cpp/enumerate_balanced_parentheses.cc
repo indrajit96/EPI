79837715bad3a389a2f6f1a4fba46ba5c4b05a57
ibm <iamindrajitb@gmail.com>
2019-06-23 11:14:16 +0530

    Back To Tracking

--- epi_judge_cpp/enumerate_balanced_parentheses.cc
+++ epi_judge_cpp/enumerate_balanced_parentheses.cc
@@ -3,10 +3,24 @@
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
-
+void driver(int left_needed, int right_needed, const string& selected, vector<string> *result) {
+  if((left_needed == 0) && (right_needed == 0)) {
+    result->emplace_back(selected);
+    return;
+  }
+  if(left_needed > 0) {
+    driver(left_needed - 1, right_needed, selected + '(' , result);
+    //able to add a left
+  }
+  if(right_needed > left_needed) {
+    driver(left_needed , right_needed - 1, selected + ')' , result);
+  }
+}
 vector<string> GenerateBalancedParentheses(int num_pairs) {
   // TODO - you fill in here.
-  return {};
+  vector<string> result;
+  driver(num_pairs , num_pairs , "" , &result);
+  return result;
 }
 
 int main(int argc, char* argv[]) {
