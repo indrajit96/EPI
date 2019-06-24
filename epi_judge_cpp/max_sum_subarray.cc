425330e78ddd982eb6ffee113da3d6374b5468f6
ibm <iamindrajitb@gmail.com>
2019-06-24 15:40:27 +0530

    DP Begins

--- epi_judge_cpp/max_sum_subarray.cc
+++ epi_judge_cpp/max_sum_subarray.cc
@@ -2,9 +2,22 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int FindMaximumSubarray(const vector<int>& A) {
+int FindMaximumSubarray(const vector<int>& a) {
+  int max_so_far = 0;
+  int max_till_here = 0;
+
+  int i = 0;
+  for(i=0;i<a.size();i++) {
+    max_till_here = max_till_here + a[i];
+    if(max_so_far < max_till_here) {
+      max_so_far = max_till_here;
+    }
+    if(max_till_here < 0) {
+      max_till_here = 0;
+    }
+  }
   // TODO - you fill in here.
-  return -1;
+  return max_so_far;
 }
 
 int main(int argc, char* argv[]) {
