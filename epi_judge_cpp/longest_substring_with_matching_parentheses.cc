17f0e473f081f3c67124fde79252dd935ca6d678
ibm <iamindrajitb@gmail.com>
2019-06-26 09:09:43 +0530

    BUG in commit

--- epi_judge_cpp/longest_substring_with_matching_parentheses.cc
+++ epi_judge_cpp/longest_substring_with_matching_parentheses.cc
@@ -5,8 +5,27 @@ using std::string;
 using std::vector;
 
 int LongestMatchingParentheses(const string& s) {
+  vector<int> dp;
+  dp.emplace_back(0);
+  int i = 1;
+  int val = 0;
+  int result = 0;
+  for(i=1;i<s.size();i++) {
+    if(s[i] == '(') {
+      dp.emplace_back(0);
+    }
+    else {
+      if(s[i-1] == '(')
+        val = 2 + dp[i-2];
+      else
+        val = 2 + dp[i-1] + dp[i- dp[i-1] - 2];
+      dp.emplace_back(val);
+      if(val > result)
+        result = val;
+    }
+  }
   // TODO - you fill in here.
-  return 0;
+  return result;
 }
 
 int main(int argc, char* argv[]) {
